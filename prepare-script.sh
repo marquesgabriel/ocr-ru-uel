1d2f71702b064d13961d67d15b0fdfe4237455db
Gilson Doi Junior <doijunior@yahoo.com.br>
2018-02-23 17:50:40 -0300

    crop and ocr on sh

---
+++ prepare-script.sh
@@ -0,0 +1,6 @@
+convert -crop 50%x32% menu.jpg
+tesseract menu-0.jpg output-0
+tesseract menu-1.jpg output-1
+tesseract menu-2.jpg output-2
+tesseract menu-3.jpg output-3
+tesseract menu-4.jpg output-4
